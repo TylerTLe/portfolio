38ba11d810c0fbc14b2ba44715c2004482501674
Tyler <tyler.le@edu.sait.ca>
2023-08-18 14:31:46 -0600

    adds experience to navbar

--- components/experience.tsx
+++ components/experience.tsx
@@ -5,10 +5,13 @@ import SectionHeading from './section-heading';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import { experiencesData } from '@/lib/data';
+import { useSectionInView } from '@/lib/hooks';
 
 export default function Experience() {
+    const { ref } = useSectionInView("Skills",);
+
   return (
-    <section id='experience'>
+    <section id='experience' ref={ref}  className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40">
         <SectionHeading>My experience</SectionHeading>
         <VerticalTimeline lineColor=''>
             {
