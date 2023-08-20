964ffe08444f6ab88bfab82077d97c3687296818
Tyler <tyler.le@edu.sait.ca>
2023-08-20 16:48:26 -0600

    alerts user if form error happens

--- components/contact.tsx
+++ components/contact.tsx
@@ -2,15 +2,13 @@
 
 import React from "react";
 import SectionHeading from "./section-heading";
-import { FaPaperPlane } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { motion } from "framer-motion";
 import { sendEmail } from "@/actions/sendEmail";
-import { experimental_useFormStatus as useFormStatus } from "react-dom";
+import SubmitBtn from "./submit-btn";
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact");
-  const { pending } = useFormStatus();
 
   return (
     <motion.section
@@ -41,7 +39,13 @@ export default function Contact() {
       <form
         className="mt-10 flex flex-col"
         action={async (formData) => {
-          await sendEmail(formData);
+          const {data, error} = await sendEmail(formData);
+          if (error) {
+            alert(error);
+            return;
+          }
+
+          alert("Email sent successfully!")
         }}
       >
         <input
@@ -59,13 +63,7 @@ export default function Contact() {
           maxLength={5000}
           placeholder="Your message"
         />
-        <button
-          className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 active:scale-105 hover:bg-gray-950"
-          type="submit"
-        >
-          Submit{" "}
-          <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />
-        </button>
+        <SubmitBtn />
       </form>
     </motion.section>
   );
