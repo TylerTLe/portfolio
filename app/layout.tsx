4fd12ce556eaa6a205c9065592693c49feb1fffd
TylerTLe <tyler.le@edu.sait.ca>
2023-12-09 14:52:44 -0700

    Add animation and background elements to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,11 +22,15 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth">
       <body
-        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
+        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90 overflow-x-hidden`}
       >
-        <div className=" bg-[#3c6e71] bg-opacity-40 dark:bg-[#acbfa4] dark:bg-opacity-40 absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-        <div className=" bg-[#353535] bg-opacity-40 dark:bg-[#e2e8ce] dark:bg-opacity-40 absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
-
+        <div className="relative">
+          <div className="animate-slide whitespace-nowrap">
+            {/* Original Elements */}
+            <div className="bg-[#67a3b6] bg-opacity-40 dark:bg-[#816be7] dark:bg-opacity-20 absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+            <div className="bg-[#b4a7d8] bg-opacity-40 dark:bg-[#59bceb] dark:bg-opacity-20 absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+          </div>
+        </div>
         <ThemeContextProvider>
           <ActionSectionContextProvider>
             <Header />
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,15 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      keyframes: {
+        slide: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-300%)' },
+        },
+      },
+      animation: {
+        slide: 'slide 20s linear infinite',
+      },
     },
   },
   plugins: [],
